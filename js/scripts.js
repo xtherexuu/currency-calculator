e76a8cab3707be0b555d69a24e617828f257524a
xtherexuu <therexuu@gmail.com>
2022-10-14 16:03:09 +0200

    added favicon and correct element name in JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,57 @@
 {
-  const fromCurrency = document.querySelector(".js-fromCurrency");
-  const toCurrency = document.querySelector(".js-toCurrency");
+  const fromCurrencyElement = document.querySelector(".js-fromCurrency");
+  const toCurrencyElement = document.querySelector(".js-toCurrency");
   const amount = document.querySelector(".js-amount");
   const resoult = document.querySelector(".js-resoult");
 
   const ifTheSameValue = () => {
-    if (fromCurrency.value === "PLN" && toCurrency.value === "PLN") {
-      resoult.innerText = `${amount.value} ${toCurrency.value}`;
-    } else if (fromCurrency.value === "EUR" && toCurrency.value === "EUR") {
-      resoult.innerText = `${amount.value} ${toCurrency.value}`;
-    } else if (fromCurrency.value === "USD" && toCurrency.value === "USD") {
-      resoult.innerText = `${amount.value} ${toCurrency.value}`;
-    } else if (fromCurrency.value === "GBP" && toCurrency.value === "GBP") {
-      resoult.innerText = `${amount.value} ${toCurrency.value}`;
+    if (fromCurrencyElement.value === "PLN" && toCurrencyElement.value === "PLN") {
+      resoult.innerText = `${amount.value} ${toCurrencyElement.value}`;
+    } else if (fromCurrencyElement.value === "EUR" && toCurrencyElement.value === "EUR") {
+      resoult.innerText = `${amount.value} ${toCurrencyElement.value}`;
+    } else if (fromCurrencyElement.value === "USD" && toCurrencyElement.value === "USD") {
+      resoult.innerText = `${amount.value} ${toCurrencyElement.value}`;
+    } else if (fromCurrencyElement.value === "GBP" && toCurrencyElement.value === "GBP") {
+      resoult.innerText = `${amount.value} ${toCurrencyElement.value}`;
     }
   };
 
   const ifValueIsPLN = () => {
-    if (fromCurrency.value === "PLN" && toCurrency.value === "EUR") {
+    if (fromCurrencyElement.value === "PLN" && toCurrencyElement.value === "EUR") {
       resoult.innerText = `${(Number(amount.value) * 0.21).toFixed(2)} EUR`;
-    } else if (fromCurrency.value === "PLN" && toCurrency.value === "USD") {
+    } else if (fromCurrencyElement.value === "PLN" && toCurrencyElement.value === "USD") {
       resoult.innerText = `${(Number(amount.value) * 0.2).toFixed(2)} USD`;
-    } else if (fromCurrency.value === "PLN" && toCurrency.value === "GBP") {
+    } else if (fromCurrencyElement.value === "PLN" && toCurrencyElement.value === "GBP") {
       resoult.innerText = `${(Number(amount.value) * 0.19).toFixed(2)} GBP`;
     }
   };
 
   const ifValueIsEUR = () => {
-    if (fromCurrency.value === "EUR" && toCurrency.value === "PLN") {
+    if (fromCurrencyElement.value === "EUR" && toCurrencyElement.value === "PLN") {
       resoult.innerText = `${(Number(amount.value) * 4.76).toFixed(2)} PLN`;
-    } else if (fromCurrency.value === "EUR" && toCurrency.value === "USD") {
+    } else if (fromCurrencyElement.value === "EUR" && toCurrencyElement.value === "USD") {
       resoult.innerText = `${(Number(amount.value) * 0.97).toFixed(2)} USD`;
-    } else if (fromCurrency.value === "EUR" && toCurrency.value === "GBP") {
+    } else if (fromCurrencyElement.value === "EUR" && toCurrencyElement.value === "GBP") {
       resoult.innerText = `${(Number(amount.value) * 0.89).toFixed(2)} GBP`;
     }
   };
 
   const ifValueIsUSD = () => {
-    if (fromCurrency.value === "USD" && toCurrency.value === "EUR") {
+    if (fromCurrencyElement.value === "USD" && toCurrencyElement.value === "EUR") {
       resoult.innerText = `${(Number(amount.value) * 1.03).toFixed(2)} EUR`;
-    } else if (fromCurrency.value === "USD" && toCurrency.value === "PLN") {
+    } else if (fromCurrencyElement.value === "USD" && toCurrencyElement.value === "PLN") {
       resoult.innerText = `${(Number(amount.value) * 4.9).toFixed(2)} PLN`;
-    } else if (fromCurrency.value === "USD" && toCurrency.value === "GBP") {
+    } else if (fromCurrencyElement.value === "USD" && toCurrencyElement.value === "GBP") {
       resoult.innerText = `${(Number(amount.value) * 0.92).toFixed(2)} GBP`;
     }
   };
 
   const ifValueIsGBP = () => {
-    if (fromCurrency.value === "GBP" && toCurrency.value === "EUR") {
+    if (fromCurrencyElement.value === "GBP" && toCurrencyElement.value === "EUR") {
       resoult.innerText = `${(Number(amount.value) * 1.12).toFixed(2)} EUR`;
-    } else if (fromCurrency.value === "GBP" && toCurrency.value === "USD") {
+    } else if (fromCurrencyElement.value === "GBP" && toCurrencyElement.value === "USD") {
       resoult.innerText = `${(Number(amount.value) * 1.09).toFixed(2)} USD`;
-    } else if (fromCurrency.value === "GBP" && toCurrency.value === "PLN") {
+    } else if (fromCurrencyElement.value === "GBP" && toCurrencyElement.value === "PLN") {
       resoult.innerText = `${(Number(amount.value) * 5.31).toFixed(2)} PLN`;
     }
   };
